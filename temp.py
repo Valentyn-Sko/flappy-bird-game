830e67a0461204f4f0f02e931cc400f9fdd5b4d1
Valentyn <valentyn@Valentyns-MacBook-Pro.local>
2020-03-12 09:57:50 +0200

    finish game

--- temp.py
+++ temp.py
@@ -1,14 +1,23 @@
 import time
-from random import randint
+from random import randint, randrange
 
 import pygame
 
 black = (0, 0, 0)
 white = (255, 255, 255)
+sunset = (253, 72, 47)
+greenyellow = (184, 255, 0)
+orange = (255, 113, 0)
+purple = (252, 67, 255)
+brightblue = (47, 228, 253)
+
+colorChoices = [sunset, greenyellow, orange, purple, brightblue]
+
 surface_width = 950
 surface_height = 500
 
 image_height = 43
+image_width = 100
 
 start_position_x = 150
 start_position_y = 200
@@ -20,21 +29,29 @@ clock = pygame.time.Clock()
 
 img = pygame.image.load('Helicopter.png')
 
-y_move = 3
+start_y_move = 5
+start_x_block_move = 12
 
 
 def helicopter(x, y, image):
     surface.blit(img, (x, y))
 
 
+def score(count):
+    font = pygame.font.Font('freesansbold.ttf', 20)
+    text = font.render('Score:' + str(count), True, white)
+    surface.blit(text, [10, 10])
+
+
 def make_text_objs(text, font):
-    text_surface = font.render(text, True, white)
+    text_surface = font.render(text, True, sunset)
     return text_surface, text_surface.get_rect()
 
 
-def blocks(x_block, y_block, block_width, block_height, gap):
-    pygame.draw.rect(surface, white, [x_block, y_block, block_width, block_height])
-    pygame.draw.rect(surface, white, [x_block, y_block + gap + block_height, block_width, block_height])
+def blocks(x_block, y_block, block_width, block_height, gap, colorChoice):
+
+    pygame.draw.rect(surface, colorChoice, [x_block, y_block, block_width, block_height])
+    pygame.draw.rect(surface, colorChoice, [x_block, y_block + gap + block_height, block_width, surface_height])
 
 
 def replay_or_quit():
@@ -51,18 +68,20 @@ def replay_or_quit():
 def main():
     x = start_position_x
     y = start_position_y
-    y_move = 3
+    y_move = start_y_move
 
-    x_block_move = 3
+    x_block_move = start_x_block_move
 
     x_block = surface_width
     y_block = 0
     block_width = 75
-    block_height = randint(0, surface_height)
+    block_height = randint(0, (surface_height / 2))
 
-    gap = image_height * 2
+    gap = image_height * 3
 
     game_over = False
+    current_score = 0
+    block_color = colorChoices[randrange(0, len(colorChoices))]
 
     while not game_over:
         for event in pygame.event.get():
@@ -77,15 +96,40 @@ def main():
 
         y += y_move
         surface.fill(black)
-        blocks(x_block, y_block, block_width, block_height, gap)
+        score(current_score)
+
+        blocks(x_block, y_block, block_width, block_height, gap, block_color)
         x_block -= x_block_move
         helicopter(x, y, img)
+        score(current_score)
 
-        if y > surface_height or y < 0:
+        if y > surface_height - 40 or y < 0:
             game_over_func()
 
+        if x_block < (-1 * block_width):
+            x_block = surface_width
+            block_height = randint(0, surface_height / 2)
+            block_color = colorChoices[randrange(0, len(colorChoices))]
+
+        if x+image_width > x_block:
+            if x < x_block + block_width:
+                if y < block_height:
+                    if x - image_width < block_width + x_block:
+                        game_over_func()
+
+        if x+image_width > x_block:
+            if y + image_height > block_height+gap:
+                if x < block_width + x_block:
+                    game_over_func()
+
+        if x <x_block and x > x_block - x_block_move:
+            current_score += 1
+            if current_score % 3 == 0:
+                x_block_move += 5
+
+
         pygame.display.update()
-        clock.tick(60)
+        clock.tick(160)
 
 
 def msg_surface(text):
